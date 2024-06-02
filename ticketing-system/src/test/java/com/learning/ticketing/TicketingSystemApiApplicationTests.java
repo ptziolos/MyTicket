1bb773af5f36210b307c6be8f6d87ae3c5f6ff4d
ptziolos <ptziolos@gmail.com>
2024-06-02 11:55:09 +0300

    Complete JWT sequrity

---
+++ ticketing-system/src/test/java/com/learning/ticketing/TicketingSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.ticketing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
